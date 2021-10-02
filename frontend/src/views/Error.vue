d96372b0d634e8091805cf48b51ccb660079092a
Lakshan Perera <contact.lakshanperera@gmail.com>
2021-10-02 23:58:51 +0530

    feat: create frontend page to handle oauth errors

---
+++ frontend/src/views/Error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>Error: {{ errorMessage }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error",
+  computed: {
+    errorMessage() {
+      return this.$route.query.errorMessage;
+    },
+  },
+};
+</script>
