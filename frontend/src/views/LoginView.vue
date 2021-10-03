6fe76a84aa3057d61ab4a22ad17a0285a9926ce5
tharikrizan <tharik.r@yahoo.com>
2021-10-03 21:41:39 +0530

    card view for logout

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    <button @click.prevent="onConnectFb">Login with facebook</button>
+    <!-- <button @click.prevent="onConnectFb">Login with facebook</button> -->
+    <b-card
+      title="Card Title"
+      img-src="https://source.unsplash.com/AGqzy-Uj3s4/1600x900"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="max-width: 100%;"
+      class="mb-2"
+      img-bottom
+    >
+      <b-card-text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </b-card-text>
+
+      <b-button href="#" @click.prevent="onConnectFb" variant="primary"
+        >Login with Facebook</b-button
+      >
+    </b-card>
   </div>
 </template>
 
